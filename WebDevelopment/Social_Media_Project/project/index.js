d5d505d420551636e2d1b44b059e7afe4a299224
SindhuKiran <you@example.comsindhusrinivas4@gmail.com>
2024-01-16 16:39:17 +0530

    Ticket Booking Aplication changes

--- WebDevelopment/Social_Media_Project/project/index.js
+++ WebDevelopment/Social_Media_Project/project/index.js
@@ -4,6 +4,15 @@ import hbs from 'hbs'
 import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import mongoose from 'mongoose'
+
+
+
+ const __filename = fileURLToPath(import.meta.url)
+
+ const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -13,6 +22,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',(req, res)=>{
     res.render("login")
@@ -94,6 +105,75 @@ app.post('/addusers',async (req,res)=>{
 
 app.get('/register',(req,res)=>{
     res.render("register")
+})
+
+// ****************** booking application code ********************//
+
+app.get('/cinema',(req,res)=>{
+    res.render("cinema",{
+        movies: moviesRes,
+        screen1: screen1Res,
+        screen2: screen2Res,
+        screen3: screen3Res
+    })
+})
+
+mongoose.connect("mongodb://127.0.0.1:27017/cinema",{
+    // useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const screen1Model = mongoose.model('screen1',{
+    seatno: {type: Number},
+    status: {type: String}
+})
+
+const screen2Model = mongoose.model('screen2',{
+    seatno: {type: Number},
+    status: {type: String}
+})
+
+const screen3Model = mongoose.model('screen3',{
+    seatno: {type: Number},
+    status: {type: String}
+})
+
+const moviesModel = mongoose.model('movies',{
+    name: {type: String},
+    rate: {type: Number},
+    screenNo: {type: Number}
+})
+
+var screen1Res
+screen1Model.find().then(function(output){
+    screen1Res = output
+})
+.catch(function (err){
+    console.log(err)
+})
+
+var screen2Res
+screen1Model.find().then(function(output){
+    screen2Res = output
+})
+.catch(function (err){
+    console.log(err)
+})
+
+var screen3Res
+screen1Model.find().then(function(output){
+    screen3Res = output
+})
+.catch(function (err){
+    console.log(err)
+})
+
+var moviesRes
+moviesModel.find().then(function(output){
+    moviesRes = output
+})
+.catch(function (err){
+    console.log(err)
 })
 
 app.listen(3000,()=>{
