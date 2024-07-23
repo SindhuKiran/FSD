9ff6aea4396883c35578f05ec9613aa4ce41bb54
SindhuKiran <you@example.comsindhusrinivas4@gmail.com>
2024-07-23 23:22:55 +0530

    javascript

---
+++ WebDevelopment/Javascript_inter/index.js
@@ -0,0 +1,7 @@
+let students = [{name:"S1", mark: 74},{name:"S2", mark: 65}, {name:"S3", mark: 80}, {name:"S4", mark: 59}, {name:"S5", mark: 80}]
+let mark=0;
+for(let i=0; i<students.length; i++){
+    mark+=students[i].mark
+}
+console.log(mark)
+
